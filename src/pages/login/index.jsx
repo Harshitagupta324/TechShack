7b7cd52d7d62d74d38240a04f53eaaa093e98ac6
void-ness <78547746+void-ness@users.noreply.github.com>
2022-11-06 04:06:07 +0530

    code: add login page

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+import Signup from './Signup';
+import Landing from './Landing';
+
+const Login = () => {
+    return (
+        <>
+            <Landing />
+            <Signup />
+        </>
+    )
+};
+
+export default Login;
